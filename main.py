fdef8113a831d2748c3cf34ad6dd7dd53880c337
sofi <gleb.lazarev20@yandex.ru>
2024-05-27 16:13:35 +0300

    first

---
+++ main.py
@@ -0,0 +1,19 @@
+import subprocess
+from fastapi import FastAPI, Request
+from starlette.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.post("/confluence-webhook")
+async def confluence_webhook_handler(request: Request):
+    payload = await request.json()
+
+    repo_url = payload["repository"]["cloneUrl"]
+
+    result = subprocess.run(["git", "clone", repo_url], capture_output=True, text=True)
+
+    if result.returncode != 0:
+        return JSONResponse({"message": f"Ошибка копирования репозитория: {result.stderr}"}, status_code=500)
+
+    return JSONResponse({"message": "Пуш успешно обработан"}, status_code=200)
